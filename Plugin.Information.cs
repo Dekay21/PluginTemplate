db93717a1db9845fa092ba59e5662ec520b0ae1f
Dekay <43781572+Dekay21@users.noreply.github.com>
2023-03-19 19:50:22 +0100

    Splitting of information and logic part in plugin

---
+++ Plugin.Information.cs
@@ -0,0 +1,17 @@
+﻿using Terraria;
+using TerrariaApi.Server;
+
+namespace PluginTemplate
+{
+    public partial class Plugin: TerrariaPlugin
+    {
+        public override string Name => "PluginTemplate";
+        public override Version Version => new(1, 0, 0);
+        public override string Author => "Dekay";
+        public override string Description => "Template to create new Tshock Plugins";
+
+        public Plugin(Main game) : base(game)
+        {
+        }
+    }
+}
