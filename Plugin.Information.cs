7532ab65f2a7477ad76434c78541dd90a780b7b9
Dekay <43781572+Dekay21@users.noreply.github.com>
2023-04-16 20:02:16 +0200

    Update Plugin.Information.cs

--- Plugin.Information.cs
+++ Plugin.Information.cs
@@ -6,7 +6,7 @@ namespace PluginTemplate
     public partial class Plugin: TerrariaPlugin
     {
         public override string Name => "PluginTemplate";
-        public override Version Version => new(1, 0, 0);
+        public override Version Version => new(0, 0, 1);
         public override string Author => "Dekay";
         public override string Description => "Template to create new Tshock Plugins";
 
